1ae62f2b59ef54becec0d22c258cf1e9b88f03a0
Kody-Smith <krsmith1999@gmail.com>
2024-06-04 09:22:39 -0600

    updates

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,7 +15,6 @@ export default function Welcome() {
         pass: "",
         confirmPass: ""
     });
-    const nav = useNavigate();
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
